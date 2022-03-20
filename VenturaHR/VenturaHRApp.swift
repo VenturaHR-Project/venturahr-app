25d8dd24428536907fc48ca2caf3e54be88ac72f
Matheus Evangelista <matheus.evangelista@g.globo>
2022-03-19 23:14:07 -0300

    Initial Commit

---
+++ VenturaHR/VenturaHRApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenturaHRApp.swift
+//  VenturaHR
+//
+//  Created by matheus.evangelista on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct VenturaHRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
