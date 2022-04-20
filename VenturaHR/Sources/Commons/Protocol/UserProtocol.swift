c4dd7d28cad2fab6b3290a2fd604bd30e6f28c7a
Matheus Evangelista <matheus.evangelista@g.globo>
2022-04-19 23:32:56 -0300

    Create UserProtocol

---
+++ VenturaHR/Sources/Commons/Protocol/UserProtocol.swift
@@ -0,0 +1,9 @@
+protocol UserProtocol {
+    var uid: String { get }
+    var name: String { get }
+    var email: String { get }
+    var password: String { get }
+    var accountType: String { get }
+    var phone: String { get }
+    var address: String { get }
+}
