0970af75d980b0885e2fc92b6c001bd5cc50d9f6
Matheus Evangelista <matheus.evangelista@g.globo>
2022-05-11 22:20:25 -0300

    Add new method for GenericUse struct and update properties

--- VenturaHR/Sources/Network/Codable/GenericUser/GenericUser.swift
+++ VenturaHR/Sources/Network/Codable/GenericUser/GenericUser.swift
@@ -1,14 +1,21 @@
+import Foundation
+
 struct GenericUser {
     var uid: String
     var name: String
-    var email: String
-    var password: String
+    var email: String?
+    var password: String?
     var accountType: String
     var phone: String
     var address: String
     var cpf: String?
     var cnpj: String?
     var corporateName: String?
+    
+    static func decode(data: Data) -> GenericUser? {
+        let decoder = JSONDecoder()
+        return try? decoder.decode(GenericUser.self, from: data)
+    }
 }
 
 extension GenericUser: Codable {
