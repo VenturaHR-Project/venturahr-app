50a6891114a3fb3fc1966f6f2da87ae312b513ee
Matheus Evangelista <matheus.evangelista@g.globo>
2022-05-14 22:02:36 -0300

    Add new decode method in IbgeState struct

--- VenturaHR/Sources/Network/Codable/ibge/IbgeState.swift
+++ VenturaHR/Sources/Network/Codable/ibge/IbgeState.swift
@@ -1,8 +1,15 @@
+import Foundation
+
 struct IbgeState {
     var id: Int
     var acronym: String
     var name: String
     var region: IbgeStateRegion
+    
+    static func decode(data: Data) -> [IbgeState]? {
+        let decoder = JSONDecoder()
+        return try? decoder.decode([IbgeState].self, from: data)
+    }
 }
 
 extension IbgeState: Codable {
