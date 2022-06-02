662e61cb2a954aca03826a89fa57fd60d75923a2
Matheus Evangelista <matheus.evangelista@g.globo>
2022-06-01 23:40:40 -0300

    Update ExpectedSkill with a new map method

--- VenturaHR/Sources/Modules/Vacancy/Model/ExpectedSkill.swift
+++ VenturaHR/Sources/Modules/Vacancy/Model/ExpectedSkill.swift
@@ -5,4 +5,17 @@ struct ExpectedSkill: Identifiable, Equatable {
     var description: String = ""
     var desiredMinimumProfile: DesiredMinimumProfile = .veryLow
     var height: Int = 1
+    
+    static func map(expectedSkills: [Skill]) -> [ExpectedSkill] {
+        let skills: [ExpectedSkill] = expectedSkills.map { skill in
+            let profile: DesiredMinimumProfile = DesiredMinimumProfile.convertValueToEnum(value: skill.desiredMinimumProfile)
+            
+            return ExpectedSkill(id: skill.id,
+                                 description: skill.description,
+                                 desiredMinimumProfile: profile,
+                                 height: skill.height)
+        }
+        
+        return skills
+    }
 }
