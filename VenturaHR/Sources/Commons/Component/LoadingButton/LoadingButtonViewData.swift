6d8f1d3325e9f40780bd216f6656a62b47154e3b
Matheus Evangelista <matheus.evangelista@g.globo>
2022-06-01 23:38:16 -0300

    Improve LoadingButtonView component

--- VenturaHR/Sources/Commons/Component/LoadingButton/LoadingButtonViewData.swift
+++ VenturaHR/Sources/Commons/Component/LoadingButton/LoadingButtonViewData.swift
@@ -1,6 +1,10 @@
+import SwiftUI
+
 struct LoadingButtonViewData {
     var action: () -> Void
     var buttonTitle: String
     var showProgress: Bool = false
     var disabled: Bool = false
+    var verticalPadding: CGFloat = 14
+    var horizontalPadding: CGFloat = 16
 }
