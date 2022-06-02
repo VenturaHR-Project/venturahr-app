f3db0e5563ac0fd531d7b2955ac0021112940ea9
Matheus Evangelista <matheus.evangelista@g.globo>
2022-06-01 23:38:56 -0300

    Update AccountType enum with new methods

--- VenturaHR/Sources/Commons/Enum/AccountType.swift
+++ VenturaHR/Sources/Commons/Enum/AccountType.swift
@@ -1,4 +1,12 @@
 enum AccountType: String, CaseIterable {
     case candidate = "Candidato"
     case company = "Empresa"
+    
+    var isCandidate: Bool {
+        return self == .candidate
+    }
+    
+    var isCompany: Bool {
+        return self == .company
+    }
 }
