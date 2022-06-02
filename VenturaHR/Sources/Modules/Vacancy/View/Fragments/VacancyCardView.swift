bf245f1b64e8a49336c91d5550d652f0817064d6
Matheus Evangelista <matheus.evangelista@g.globo>
2022-06-01 23:37:39 -0300

    Organize folder structure

---
+++ VenturaHR/Sources/Modules/Vacancy/View/Fragments/VacancyCardView.swift
@@ -0,0 +1,20 @@
+//
+//  VacancyCardView.swift
+//  VenturaHR
+//
+//  Created by Matheus Evangelista on 31/05/22.
+//
+
+import SwiftUI
+
+struct VacancyCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VacancyCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        VacancyCardView()
+    }
+}
